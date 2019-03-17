48001a6ee1512364fbc299132a8ca45fbce23b54
Dillon Tuck <robertdill.tuck@gmail.com>
2019-03-16 20:06:12 -0400

    Modified to Jascha's portfolio page now

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,8 +1,5 @@
 import $ from 'jquery'
 
-const $helloWorld = $('<div class="header">HELLO WORLD!!!<div>');
-
 $(function(){
-  $('#app-container').append($helloWorld);
-  $('#app-container').append('<div class="glasses-img">s</div>');
-});
+    console.log('Hello World!');
+})
